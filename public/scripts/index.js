1c27834f9d76e1519e2f87bb896191806db654a3
Juan Daniel Forner <jdanielforga@gmail.com>
2023-12-19 16:42:26 +0100

    added handlers to navigation buttons

---
+++ public/scripts/index.js
@@ -0,0 +1,8 @@
+
+document.getElementById('previous').addEventListener('click', () => {
+    console.log('previous');
+});
+
+document.getElementById('next').addEventListener('click', () => {
+    console.log('next');
+});
